fdd20861ff9e3925a61e9a90478bd26b76b5634a
snowypy <git@snowyjs.lol>
2024-12-28 20:07:12 +0000

    Export close and close request commands in command index for ticket management

---
+++ dist/commands/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.closeRequestCommand = exports.closeCommand = void 0;
+var close_1 = require("./close");
+Object.defineProperty(exports, "closeCommand", { enumerable: true, get: function () { return close_1.closeCommand; } });
+var closeRequest_1 = require("./closeRequest");
+Object.defineProperty(exports, "closeRequestCommand", { enumerable: true, get: function () { return closeRequest_1.closeRequestCommand; } });
